dd0b1e43f184c893f3b0b7b82cbe8e4dd24dba16
Edima <edimaudo@gmail.com>
2025-08-25 16:19:25 +0000

    renamed from streamlit_app

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+pg = st.navigation([
+    st.Page("About.py"),
+    st.Page("Labour_Law_Information.py"),
+    st.Page("Contract_Insights.py"),
+    st.Page("Lawyer_Information.py"),
+])
+pg.run()
