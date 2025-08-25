6b80a4526a0771fd339ae0d499a8322513faa4fd
Edima <edimaudo@gmail.com>
2025-08-25 16:27:34 +0000

    update

---
+++ About.py
@@ -0,0 +1,13 @@
+from utils import *
+
+st.title(APP_NAME)
+st.header(ABOUT_HEADER)
+
+st.markdown(
+    """
+    This project provides the opportunity for employees to analyze employment or labor contracts and see if there are any areas of concern.  
+    The goal is to better empower employees especially in an uncertain job market.
+    It is built using [streamlit](https://streamlit.io/cloud), [TIDB](https://www.pingcap.com/) and [Google Gemini](https://aistudio.google.com/).  
+    **N/B**: This app is not a substitute for a qualified lawyer. 
+    """
+)
